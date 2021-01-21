5ba0aa83cbf4dcd232da0690845c568ccc6ff776
saif <mohamedhawas123@gmail.com>
2021-01-21 15:05:02 +0200

    models

---
+++ store/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+app_name = 'store'
+
+
+urlpatterns = [
+    path('', views.product_list, name="product_list" ),
+    path('<slug:category_slug>/', views.product_list, name="product_list_by_category"),
+    path('<int:id>/<int:slug>/', views.product_detail, name="product_detail")
+]
---
+++ store/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, get_object_or_404
+from .models  import Category, Product
+
+
+def product_list(request, category_slug=None):
+    category = None
+    products = Product.objects.filter(available=True)
+    categories = Category.objects.all()
+    if category_slug:
+        category = get_object_or_404(Category, slug= category_slug)
+        products = products.filter(category=category)
+
+    return render(request, 'store/product/list.html', {'categories':categories, 'products':products})
+
+
+
+def product_detail(request, slug, id):
+    product = get_object_or_404(Product, slug=slug, id=id, available=True)
+    return render(request, 'store/product/list.html', {'product': product})
+
