172b07c66e928e395794cadc4c791fa348f1b52b
saif <mohamedhawas123@gmail.com>
2021-01-23 14:57:50 +0200

    product-list and detail

--- store/models.py
+++ store/models.py
@@ -15,14 +15,14 @@ class Category(models.Model):
         return self.name
     
     def get_absolute_url(self):
-        return reverse('shop:product_list_by_category', kwargs={'category_slug': self.slug})
+        return reverse('store:product_list_by_category', kwargs={'category_slug': self.slug})
 
 
 class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True)
-    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    image = models.ImageField(upload_to='media/products/%Y/%m/%d', blank=True)
     descriptopn = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
@@ -37,7 +37,7 @@ class Product(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('shop:product_detail', kwargs={'slug': self.slug, 'id': self.id})
+        return reverse('store:product_detail', kwargs={'id': self.id, 'slug': self.slug})
 
 
     
--- store/views.py
+++ store/views.py
@@ -10,11 +10,11 @@ def product_list(request, category_slug=None):
         category = get_object_or_404(Category, slug= category_slug)
         products = products.filter(category=category)
 
-    return render(request, 'store/product/list.html', {'categories':categories, 'products':products})
+    return render(request, 'store/product/list.html', {'category': category,'categories':categories, 'products':products})
 
 
 
 def product_detail(request, slug, id):
     product = get_object_or_404(Product, slug=slug, id=id, available=True)
-    return render(request, 'store/product/list.html', {'product': product})
+    return render(request, 'store/product/detail.html', {'product': product})
 
