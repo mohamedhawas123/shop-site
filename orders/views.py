6662be737ba47bfac91fa4baa2bbb5b92dae45ac
saif <mohamedhawas123@gmail.com>
2021-02-08 16:02:56 +0200

    customize admin

--- orders/views.py
+++ orders/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 from cart.cart import Cart
 from .forms import OrderCreateForm
-from .models import OrderItem
+from .models import OrderItem, Order
 from .tasks import order_created
 from django.urls import reverse
-
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import get_object_or_404
 
 
 def order_create(request):
@@ -25,3 +26,12 @@ def order_create(request):
         
     
     return render(request, 'orders/order/create.html', {'form': form } )
+
+
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request,
+                  'admin/orders/order/detail.html',
+                  {'order': order})
