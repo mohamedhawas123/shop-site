5a2867bf38c317b6c441c1fbfb237ea62c8eed64
mohamed hawas <mohamedhawas123@gmail.com>
2023-01-01 01:40:07 +0200

    Update views.py

--- orders/views.py
+++ orders/views.py
@@ -9,7 +9,6 @@ from django.shortcuts import get_object_or_404
 from django.conf import settings
 from django.http import HttpResponse
 from django.template.loader import render_to_string
-import weasyprint
 
 
 
@@ -53,7 +52,5 @@ def admin_order_pdf(request, order_id):
                             {'order': order})
     response = HttpResponse(content_type='application/pdf')
     response['Content-Disposition'] = f'filename=order_{order.id}.pdf'
-    weasyprint.HTML(string=html).write_pdf(response,
-        stylesheets=[weasyprint.CSS(
-            settings.STATIC_ROOT + 'css/pdf.css')])
+   
     return response
