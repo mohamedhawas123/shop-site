65856c2c39efc828030061c224e266e4101b1ae1
mohamed hawas <mohamedhawas123@gmail.com>
2023-01-01 01:33:43 +0200

    Update tasks.py

--- payment/tasks.py
+++ payment/tasks.py
@@ -24,9 +24,7 @@ def payment_completed(order_id):
     # generate PDF
     html = render_to_string('orders/order/pdf.html', {'order': order})
     out = BytesIO()
-    stylesheets=[weasyprint.CSS(settings.STATIC_ROOT + 'css/pdf.css')]
-    weasyprint.HTML(string=html).write_pdf(out,
-                                          stylesheets=stylesheets)
+    
     # attach PDF file
     email.attach(f'order_{order.id}.pdf',
                  out.getvalue(),
