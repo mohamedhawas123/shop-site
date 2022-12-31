3793fee1925f26978906f01cfc72863d01e522e6
mohamed hawas <mohamedhawas123@gmail.com>
2023-01-01 01:35:24 +0200

    Update tasks.py

--- payment/tasks.py
+++ payment/tasks.py
@@ -1,6 +1,5 @@
 from io import BytesIO
 from celery import task
-import weasyprint
 from django.template.loader import render_to_string
 from django.core.mail import EmailMessage
 from django.conf import settings
